c4d3396677d1537beb6213c84453dd69745cfa5a
Amin Adibi <adibi@alumni.ubc.ca>
2018-02-02 14:07:42 -0800

    fixing package structure

--- R/sparkTable.R
+++ R/sparkTable.R
@@ -1,14 +1,13 @@
 # load data (in long format)
 data(AT_Soccer, package = "sparkTable")
 # first three observations to see the structure of the data
-head(AT_Soccer, 3)
 
 # prepare content
 content <- list(
      function(x) { sum(x) },
      function(x) { round(sum(x), 2) },
      function(x) { round(sum(x), 2) },
-     newSparkLine(lineWidth = 2, pointWidth = 6), newSparkBar())
+     sparkTable::newSparkLine(lineWidth = 2, pointWidth = 6), sparkTable::newSparkBar())
 
 names(content) <- c("Points", "ShotGoal", "GetGoal", "GoalDiff", "WinLose")
 
@@ -16,19 +15,19 @@ names(content) <- c("Points", "ShotGoal", "GetGoal", "GoalDiff", "WinLose")
 vars <- c("points", "shotgoal", "getgoal", "goaldiff", "wl")
 
 # create the sparkTable object
-stab <- newSparkTable(dataObj = AT_Soccer, tableContent = content, varType = vars)
+stab <- sparkTable::newSparkTable(dataObj = AT_Soccer, tableContent = content, varType = vars)
 
 
-statcope <- read_sas("H:/SAS Projects/Exacerbation Three Trials/3DataSets/STATCOPE/statcope.sas7bdat",
+statcope <- haven::read_sas("H:/SAS Projects/Exacerbation Three Trials/3DataSets/STATCOPE/statcope.sas7bdat",
                      NULL)
 age <- statcope$staage
-sline <- newSparkLine (value=age)
-slineIQR <- newSparkLine (value = age, showIQR = TRUE)
-sbar <- newSparkBar (value = age, barSpacingPerc = 5)
-sbox <- newSparkBox (value = age)
-shist <- newSparkHist(value = age)
+sline <- sparkTable::newSparkLine (value=age)
+slineIQR <- sparkTable::newSparkLine (value = age, showIQR = TRUE)
+sbar <- sparkTable::newSparkBar (value = age, barSpacingPerc = 5)
+sbox <- sparkTable::newSparkBox (value = age)
+shist <- sparkTable::newSparkHist(value = age)
 
-#plot(shist)
-export(sbox, outputType = "png", filename = "./test")
-
+sparkTable::plot(shist)
+sparkTable::export(sbox, outputType = "png", filename = "./figures/test")
+getwd()
 
